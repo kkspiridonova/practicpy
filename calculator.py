9473b2128f71222f0b814eb61ed49a81dab21c13
kkspiridonova <kkspiridonova@yandex.ru>
2023-09-28 14:50:47 +0300

    апдейт

--- calculator.py
+++ calculator.py
@@ -12,66 +12,68 @@ print('9. косинус')
 print('10. тангенс')
 opt = 1
 while (0 < opt <= 10):
-    opt = int(input("Выберите опцию:"))
-    match opt:
-        case 1:
-            num1 = float(input("Введите первое число: "))
-            num2 = float(input("Введите второе число: "))
-            num3 = num1 + num2
-            print("Результат:", num3)
-        case 2:
-            num1 = float(input("Введите первое число: "))
-            num2 = float(input("Введите второе число: "))
-            num3 = num1 - num2
-            print("Результат:", num3)
-        case 3:
-            num1 = float(input("Введите первое число: "))
-            num2 = float(input("Введите второе число: "))
-            num3 = num1 * num2
-            print("Результат:", num3)
-        case 4:
-            num1 = float(input("Введите первое число: "))
-            num2 = float(input("Введите второе число: "))
-            if num2 == 0:
-                print("Ошибка!")
-            else:
-                num3 = num1 / num2
+    try:
+        opt = int(input("Выберите опцию:"))
+        match opt:
+            case 1:
+                num1 = float(input("Введите первое число: "))
+                num2 = float(input("Введите второе число: "))
+                num3 = num1 + num2
                 print("Результат:", num3)
-        case 5:
-            num1 = float(input("Введите число: "))
-            num2 = float(input("Введите степень: "))
-            if num1 == 0 and num2 < 0:
-                print ("Ошибка!")
-            else:
-                num3 = num1 ** num2
+            case 2:
+                num1 = float(input("Введите первое число: "))
+                num2 = float(input("Введите второе число: "))
+                num3 = num1 - num2
                 print("Результат:", num3)
-        case 6:
-            num1 = float(input("Введите число: "))
-            if num1 < 0:
-                print("Ошибка!")
-            else:
-                num3 = math.sqrt(num1)
+            case 3:
+                num1 = float(input("Введите первое число: "))
+                num2 = float(input("Введите второе число: "))
+                num3 = num1 * num2
                 print("Результат:", num3)
-        case 7:
-            num1 = int(input("Введите число: "))
-            if num1 < 0 or num1 == float:
-                print("Ошибка!")
-            else:
-                num3 = math.factorial(num1)
+            case 4:
+                num1 = float(input("Введите первое число: "))
+                num2 = float(input("Введите второе число: "))
+                if num2 == 0:
+                    print("Ошибка!")
+                else:
+                    num3 = num1 / num2
+                    print("Результат:", num3)
+            case 5:
+                num1 = float(input("Введите число: "))
+                num2 = float(input("Введите степень: "))
+                if num1 == 0 and num2 < 0:
+                    print ("Ошибка!")
+                else:
+                    num3 = num1 ** num2
+                    print("Результат:", num3)
+            case 6:
+                num1 = float(input("Введите число: "))
+                if num1 < 0:
+                    print("Ошибка!")
+                else:
+                    num3 = math.sqrt(num1)
+                    print("Результат:", num3)
+            case 7:
+                num1 = int(input("Введите число: "))
+                if num1 < 0 or num1 == float:
+                    print("Ошибка!")
+                else:
+                    num3 = math.factorial(num1)
+                    print("Результат:", num3)
+            case 8:
+                num1 = float(input("Введите угол (в градусах): "))
+                num3 = math.sin(math.radians(num1))
                 print("Результат:", num3)
-        case 8:
-            num1 = float(input("Введите угол (в градусах): "))
-            num3 = math.sin(math.radians(num1))
-            print("Результат:", num3)
-        case 9:
-            num1 = float(input("Введите угол(в градусах): "))
-            num3 = math.cos(math.radians(num1))
-            print("Результат:", num3)
-        case 10:
-            num1 = float(input("Введите число: "))
-            num3 = math.tan(math.radians(num1))
-            print("Результат:", num3)
-
+            case 9:
+                num1 = float(input("Введите угол(в градусах): "))
+                num3 = math.cos(math.radians(num1))
+                print("Результат:", num3)
+            case 10:
+                num1 = float(input("Введите число: "))
+                num3 = math.tan(math.radians(num1))
+                print("Результат:", num3)
+    except ValueError:
+                    print('Это не число! Попробуйте еще раз.')
 
 
 
